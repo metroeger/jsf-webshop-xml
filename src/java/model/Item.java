b85fce05cfabcdbcd142a35202b9095f0d473e27
Agi <Agi@Agi-PC>
2019-01-13 10:49:51 +0100

    add (cart)Item class

---
+++ src/java/model/Item.java
@@ -0,0 +1,41 @@
+
+package model;
+
+/**
+ *
+ * @author metroeger
+ */
+public class Item {
+    
+    private CD product;
+    private int quantity;
+    
+    public Item(){
+        
+    }
+    
+    public Item(CD product, int quantity){
+        this.product=product;
+        this.quantity=quantity;
+    }
+    
+    public double getTotalPrice(){
+        return quantity*product.getPrice();
+    }
+    
+    public CD getProduct(){
+        return product;
+    }
+    
+    public void setProduct(CD product){
+        this.product=product;
+    }
+    
+    public int getQuantity(){
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+}
