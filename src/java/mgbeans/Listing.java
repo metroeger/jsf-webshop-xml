f57e16fe5a1a07c343624a0a67822541126829b4
Agi <Agi@Agi-PC>
2019-01-13 10:22:42 +0100

    add managed bean which reads from XML file

---
+++ src/java/mgbeans/Listing.java
@@ -0,0 +1,77 @@
+package mgbeans;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import model.CD;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+@ManagedBean
+@SessionScoped
+public class Listing {
+
+    private List<CD> cdList;
+    private String error;
+
+    public Listing() {
+
+        error = "";
+
+        SAXBuilder saxBuilder = new SAXBuilder();
+        File file = new File("C:\\webshop\\cd_catalog.xml");
+        Document document;
+
+        cdList = new ArrayList<>();
+
+        try {
+
+            document = saxBuilder.build(file);
+            Element root = document.getRootElement();
+            List<Element> elementList = root.getChildren();
+
+            for (Element e : elementList) {
+                String title = e.getChildText("TITLE");
+
+                CD cd = new CD(
+                        e.getChildText("TITLE"),
+                        e.getChildText("ARTIST"),
+                        e.getChildText("COUNTRY"),
+                        e.getChildText("COMPANY"),
+                        Double.parseDouble(e.getChildText("PRICE")),
+                        Integer.parseInt(e.getChildText("YEAR"))
+                );
+                cdList.add(cd);
+            }
+        } catch (JDOMException ex) {
+            error += ex;
+        } catch (IOException ex) {
+            error += ex;
+        }
+        
+        
+    }
+
+    public List<CD> getCdList() {
+        return cdList;
+    }
+
+    public void setCdList(List<CD> cdList) {
+        this.cdList = cdList;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
